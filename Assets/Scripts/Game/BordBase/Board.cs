2757d7019c04fc5d14e1762f9bb0355ad789cee5
cem.erdogan <cem.cemerdogan97@gmail.com>
2024-07-30 22:26:24 +0300

    bind factories

--- Assets/Scripts/Game/BordBase/Board.cs
+++ Assets/Scripts/Game/BordBase/Board.cs
@@ -3,5 +3,11 @@ using Zenject;
 
 public class Board : MonoBehaviour
 {
-    [Inject] Cell.CellFactory _cellFactory;
+    [SerializeField] private Transform cellParent;
+    
+    [Inject] private Cell.CellFactory _cellFactory;
+    
+    public int Rows { get; private set; }
+    public int Cols { get; private set; }
+    public Cell[,] Cells { get; private set; }
 }
