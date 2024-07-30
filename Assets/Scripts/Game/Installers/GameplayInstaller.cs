a09366d6b679c8b5cc3789c2338dff9e9e705998
cem.erdogan <cem.cemerdogan97@gmail.com>
2024-07-30 22:17:15 +0300

    updated board and gameplayinstaller

---
+++ Assets/Scripts/Game/Installers/GameplayInstaller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "GameplayInstaller", menuName = "Installers/GameplayInstaller")]
+public class GameplayInstaller : ScriptableObjectInstaller<GameplayInstaller>
+{
+    [SerializeField] private Cell cellPrefab;
+    
+    public override void InstallBindings()
+    {
+        Container.BindFactory<Cell, Cell.CellFactory>().FromComponentInNewPrefab(cellPrefab).AsSingle();
+    }
+}
