f831464a819e1dc0454f6b86d55907556f997cb2
cem.erdogan <cem.cemerdogan97@gmail.com>
2024-07-31 21:31:57 +0300

    created cell/item base prefab
    bind signalbus

--- Assets/Scripts/Game/Installers/GameplayInstaller.cs
+++ Assets/Scripts/Game/Installers/GameplayInstaller.cs
@@ -9,6 +9,8 @@ public class GameplayInstaller : ScriptableObjectInstaller<GameplayInstaller>
     
     public override void InstallBindings()
     {
+        SignalBusInstaller.Install(Container);
+        
         Container.BindFactory<Cell, Cell.CellFactory>()
             .FromComponentInNewPrefab(cellPrefab)
             .AsSingle();
